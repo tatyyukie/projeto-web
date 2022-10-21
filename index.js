0048155f62995a0d256d8a4ac15d718b61bfd5cf
raulcarlosdias <raul.carlos@hotmail.com>
2022-10-21 19:31:08 -0300

    CRIANDO FUNCÃO DE LOGIN COM API REQRES

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
 const email_input = document.querySelector('#email-input');
 const password_input = document.querySelector('#password-input');
 const login_button  = document.querySelector('.btn-login');
-
 
 //Elementos da API OpenWeather
 const api_key = '6235a22f6692e222b786ecbb1a900192';
@@ -12,6 +10,25 @@ const city_label = document.querySelector('.city-name');
 const temperature_label = document.querySelector('.temperature');
 const weather = document.querySelector('.weather-content');
 const weather_icon = document.querySelector('.weather-icon');
+
+login_button.addEventListener('click', () => {
+    let email = email_input.value;
+    let password = password_input.value;
+  
+    if(email.length >= 3 && password.length >= 3) {
+        axios.post('https://reqres.in/api/login', {
+            email: email,
+            password: password
+        })
+        .then(() => {
+            document.querySelector('.content.search').classList.remove('hidden');
+            document.querySelector('.content.login').classList.add('hidden');
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+});
 
 //Função da API OpenWeather
 search_button.addEventListener('click', () => {
