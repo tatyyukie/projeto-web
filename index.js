9e9536e7abad951d6e3da2a19be69ac8f4b89652
tatyyukie <tatyy-yukie@hotmail.com>
2022-11-03 23:14:11 -0300

    Adicionando validação do campo de busca no js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const city_input = document.querySelector('.search-content .form-input');
 const search_button = document.querySelector('.btn-search');
 const city_label = document.querySelector('.city-name');
 const temperature_label = document.querySelector('.temperature');
-const weather = document.querySelector('.weather-content');
+const weather_content = document.querySelector('.weather-content');
+const weather_info = document.querySelector('.weather-item');
 const weather_icon = document.querySelector('.weather-icon');
 
 logout_button.addEventListener('click', () => {
@@ -46,18 +47,24 @@ login_button.addEventListener('click', () => {
 //Função da API OpenWeather
 search_button.addEventListener('click', () => {
     let city_name = city_input.value;
-    if(city_name != ''){
+
+    if(city_name.length >= 3){
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&APPID=${api_key}&units=metric`)
         .then(response => {
             city_label.innerHTML = response.data.name;
             temperature_label.innerHTML = Math.round(response.data.main.temp) +'°';
             weather_icon.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-            weather.classList.remove('hidden');
+            weather_info.classList.remove('hidden');
+            weather_content.classList.remove('hidden');
         })
         .catch(error => {
             city_label.innerHTML = 'Insira uma cidade válida';
-            temperature_label.innerHTML = '';
-            weather.classList.remove('hidden');
+            weather_info.classList.add('hidden');
+            weather_content.classList.remove('hidden');
         })
+    }else{
+        city_label.innerHTML = 'Insira uma cidade válida';
+        weather_info.classList.add('hidden');
+        weather_content.classList.remove('hidden');
     }
 });
