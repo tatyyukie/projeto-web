c66826e413dda22b902867402bd3f29f1d7913f6
tatyyukie <tatyy-yukie@hotmail.com>
2022-12-04 16:32:25 -0300

    Adicionando Bootstrap

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoClient = require('mongodb').MongoClient;
-
-mongoClient.connect('mongodb://127.0.0.1:27017',
-                    { useNewUrlParser: true }, (err, client) => {
-    if (err) throw err;
-    const db = client.db('mongo-web');
-    console.log(db);
-});
