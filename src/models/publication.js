23a41226af9c3be05bdf0ad8d069841861fb0acf
tatyyukie <tatyy-yukie@hotmail.com>
2022-12-08 14:01:27 -0300

    Alterando model publication

--- src/models/publication.js
+++ src/models/publication.js
@@ -9,12 +9,7 @@ const PublicationSchema = new mongoose.Schema({
     img: {
         type: String,
         required: true,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        require: true,
-    },
+    }
 });
 
 const Publication = mongoose.model('Publication', PublicationSchema);
