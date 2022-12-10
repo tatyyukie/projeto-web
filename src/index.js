51866f966d554a9fecbe04d9ab5e9bb5afdd17b7
raulcarlosdias <raul.carlos@hotmail.com>
2022-12-09 22:02:22 -0300

    Configurando Rota para pasta Public

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const router = express.Router();
-const bodyParser = require('body-parser');
+const path = require("path");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const auth = require('./middlewares/auth');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname + '../../index.html'));
+app.set("views", path.join(dirname, "template"));
+app.set("view engine", "hbs");
+hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this);
 });
 
-app.get('/publications', async (req, res) => {
-    res.sendFile(path.join(__dirname + '../../publications.html'));
+app.use("/public", express.static(path.join(dirname, "../public")));
+
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname + "../../index.html"));
 });
 
-require('./controllers/index')(app);
+require("./controllers/index")(app);
 
 app.listen(3000);
