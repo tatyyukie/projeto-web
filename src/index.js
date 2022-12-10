c7f309ddf5b0664d25f117f353a63347d8cc9633
tatyyukie <tatyy-yukie@hotmail.com>
2022-12-09 23:01:25 -0300

    Corrigindo diretório e atualizando pág para upload

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const auth = require('./middlewares/auth');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set("views", path.join(dirname, "template"));
+app.set("views", path.join(__dirname, "template"));
 app.set("view engine", "hbs");
 hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
     return arg1 == arg2 ? options.fn(this) : options.inverse(this);
 });
 
-app.use("/public", express.static(path.join(dirname, "../public")));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 app.get("/", async (req, res) => {
     res.sendFile(path.join(__dirname + "../../index.html"));
